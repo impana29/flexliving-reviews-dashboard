e13ba9ab2b6a39d40fbde67245830025fe29f059
IMPANA <56184904+impana29@users.noreply.github.com>
2025-09-05 00:34:26 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
-import "../styles/globals.css";
-import Layout from "../components/Layout";
+import { Analytics } from '@vercel/analytics/react';
+import '../styles/globals.css';
 
-export default function App({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
+    <>
       <Component {...pageProps} />
-    </Layout>
+      <Analytics />
+    </>
   );
 }
+
+export default MyApp;
 
